a79f2d2ef96135b2431183b3bd9bd5b52c71e7d5
SylvesterSarpong <apps@bothofus.se>
2022-02-23 12:14:50 +0000

    handling user csv data

---
+++ src/pages/Map_Your_Data/MapYourData.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material";
+const MapYourData = () => {
+  return (
+    <Container maxWidth="lg">
+      <Typography style={{ marginTop: "20px" }} variant="h5">
+        Map your Data
+      </Typography>
+      <div>
+        <FormControl>
+          <Grid>
+            <Grid item>
+              <Typography style={{ marginTop: "20px" }} variant="p">
+                Map your Data
+              </Typography>
+              <Input id="store-name" />
+            </Grid>
+          </Grid>
+        </FormControl>
+      </div>
+    </Container>
+  );
+};
+
+export default MapYourData;
