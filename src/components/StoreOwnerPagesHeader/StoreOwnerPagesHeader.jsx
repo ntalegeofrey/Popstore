e708df4457028e663aa82129d6fd0d1f247a16c9
Emehado <emehado@gmail.com>
2022-02-24 09:17:58 +0000

    Add close button to storeOwner pages

--- src/components/StoreOwnerPagesHeader/StoreOwnerPagesHeader.jsx
+++ src/components/StoreOwnerPagesHeader/StoreOwnerPagesHeader.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 
 import LogoutButton from "../../components/Logout Button/LogoutButton";
 import useActions from "./actions";
 
 const PageHeader = styled(Typography)({
   textTransform: "capitalize",
-  fontWeight: 600,
+  fontWeight: 500,
+  fontSize: 22,
+  display: "inline-block",
+  marginRight: "2rem",
 });
 
 const Header = styled(Box)({
@@ -25,6 +30,9 @@ const StoreOwnerPagesHeader = () => {
     <Header>
       <Box>
         <PageHeader variant="h4">{pageTitle} list</PageHeader>
+        <span>
+          <Link to="/my-popstore">Close</Link>
+        </span>
         <Typography>{storeName}</Typography>
       </Box>
       <LogoutButton />
