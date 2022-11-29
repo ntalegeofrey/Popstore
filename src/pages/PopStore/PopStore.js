d6d34c66a60a87ee61c9288bc278a5a5111276ab
Karl-Petter Åkesson <karl-petter@yelloworb.com>
2022-11-29 20:57:22 +0100

    Add email to store owner on order

--- src/pages/PopStore/PopStore.js
+++ src/pages/PopStore/PopStore.js
@@ -169,6 +169,31 @@ const PopStore = () => {
         setEmail("");
         setPhone("");
         setSubmitting(false);
+
+        let newOrderEmail = `
+            <!doctype html>
+            <html lang="en">
+            <head>
+            <style>
+               body{
+                    font-family: 'Arial', Helvetica, Arial, Lucida, sans-serif;
+               }
+            </style>
+            <title>New PopStore Order</title>
+            </head>
+            <body>
+            <h1>Order</h1>
+            <p>A new order has been placed on <b>${store.storeName}</b>. You can view your order by visiting the following link:</p>
+            <p><a href="${storeLink}/order/${ownerId}/${storeId}/${orderRef.id}">View Order</a></p>
+            <p>&nbsp;</p>
+            <p>Regards</p>
+            <p>PopStore Team</p>
+            </body>
+            </html>
+            `;
+        if (isEmail(store.storeOwner)) {
+            sendMail(store.storeOwner, "New PopStore Order", newOrderEmail);
+        }
     }
 
   if (loading) return <Loading />;
