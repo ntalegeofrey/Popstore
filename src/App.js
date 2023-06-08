f7706d07de093912da00ff1735f3a9f11422ac1c
Ntale Geofrey <ntalegeofrey@gmail.com>
2023-06-08 12:27:07 +0300

    testing appbar padding

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Navigation />
       <MainContainer component={"main"}>
         {/* puts content below appbar */}
-        <Box sx={{ height: "15px" }}></Box>
+        <Box sx={{ height: "30px" }}></Box>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route exact path="/popstore/all" element={<MyPopstore />} />
--- src/components/Styles/styledAppBar.js
+++ src/components/Styles/styledAppBar.js
@@ -24,7 +24,7 @@ export const StyledToolBar = styled(Toolbar)(({ theme }) => ({
     marginRight: 0,
   },
   [theme.breakpoints.up("md")]: {
-    marginLeft: "20px",
+    marginLeft: "35px",
     marginRight: "25px",
   },
 }));
