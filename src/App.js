8dc120308351fb7634b723291da0b4670acf67cf
Ntale Geofrey <ntalegeofrey@gmail.com>
2023-06-08 12:16:09 +0300

    testing appbar padding

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Navigation />
       <MainContainer component={"main"}>
         {/* puts content below appbar */}
-        <Box sx={{ height: "45px", mt: 2 }}></Box>
+        <Box sx={{ height: "25px", mt: 2 }}></Box>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route exact path="/popstore/all" element={<MyPopstore />} />
