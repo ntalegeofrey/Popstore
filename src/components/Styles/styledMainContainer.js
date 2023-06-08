dae7486ca978d3b291e46c70723bd3be49c2a2d0
Ntale Geofrey <ntalegeofrey@gmail.com>
2023-06-08 12:21:08 +0300

    testing appbar padding

--- src/components/Styles/styledMainContainer.js
+++ src/components/Styles/styledMainContainer.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 
 // Styled main container
 export const MainContainer = styled(Box)(({ theme }) => ({
-  // padding: theme.spacing(3),
+  padding: theme.spacing(3),
   minHeight: "100vh",
   width: "100%",
   backgroundColor: theme.palette.background,
